5fdcc49311dcd32b877087496b79f7e60b1af245
Ed Zynda <ezynda3@gmail.com>
2013-11-11 15:36:19 -0600

    Created boilerplate code.

---
+++ app/controllers/MessagesController.php
@@ -0,0 +1,36 @@
+<?php
+
+class MessagesController extends BaseController {
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+        return View::make('messages.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+        return View::make('messages.show');
+	}
+
+}
---
+++ app/database/seeds/MessagesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+class MessagesTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		// DB::table('messages')->truncate();
+
+		$messages = array(
+
+		);
+
+		// Uncomment the below to run the seeder
+		// DB::table('messages')->insert($messages);
+	}
+
+}
