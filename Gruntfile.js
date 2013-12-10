75e0381667376015765fc23e3872c5d3071f00d1
Ed Zynda <ezynda3@gmail.com>
2013-12-10 14:35:23 -0600

    Installed Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+//Gruntfile
+module.exports = function(grunt) {
+
+    //Initializing the configuration object
+    grunt.initConfig({
+        
+        concat: {
+
+        },
+
+        less: {
+
+        },
+
+        uglify: {
+
+        },
+        
+        phpunit: {
+
+        },
+
+        watch: {
+
+        }
+
+    });
+
+}
