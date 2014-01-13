c47080ff53464ed1b02ac90b3f150cbde63705c0
Ed Zynda <ezynda3@gmail.com>
2014-01-13 16:05:15 -0600

    fixed config

--- app/config/app.php
+++ app/config/app.php
@@ -113,8 +113,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
-        'Way\Generators\GeneratorsServiceProvider',
-
 	),
 
 	/*
