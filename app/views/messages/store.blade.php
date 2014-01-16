d556c0b13395c272603a12831e28b5a06a1332dc
Ed Zynda <ezynda3@gmail.com>
2014-01-16 15:53:18 -0600

    prettier link

--- app/views/messages/store.blade.php
+++ app/views/messages/store.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="alert alert-success">
     Your message has been saved.
-    Here is the URL <a href="{{ secure_url('/' . $url . '/' . $key) }}">{{ secure_url('/' . $url . '/' . $key)}}</a>
+    Here is the URL <a href="{{ secure_url('/' . $url . '/' . $key) }}" class="byline">{{ secure_url('/' . $url . '/' . $key)}}</a>
 </div>
 @stop
